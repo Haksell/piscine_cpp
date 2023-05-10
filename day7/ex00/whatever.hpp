c2a5cc24a5a93ec591d5b150d6e60225fe810138
Axel Brisse <axel.brisse@gmail.com>
2023-05-10 14:59:54 +0200

    day7/ex00

---
+++ day7/ex00/whatever.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <iostream>
+
+template <typename T> const T &min(const T &a, const T &b) {
+    return a < b ? a : b;
+}
+
+template <typename T> const T &max(const T &a, const T &b) {
+    return a > b ? a : b;
+}
+
+template <typename T> void swap(T &a, T &b) {
+    T c = a;
+    a = b;
+    b = c;
+}
